67e420de60fd45121996984915493e5a3f729c73
Itamar Junior <itjunior@gmail.com>
2024-04-12 20:30:28 +0100

    Update root URL in Main.jsx

--- resources/js/components/Main.jsx
+++ resources/js/components/Main.jsx
@@ -4,7 +4,7 @@ import '../../css/app.css';
 import ChatBox from "./ChatBox.jsx";
 
 if (document.getElementById('main')) {
-    const rootUrl = "http://127.0.0.1:8000";
+    const rootUrl = "https://chat.server.itjunior.dev";
     
     ReactDOM.createRoot(document.getElementById('main')).render(
         <React.StrictMode>
